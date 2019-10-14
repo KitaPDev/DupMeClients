aebcd707a945eb4f2b64c7b220217ed03b666e1f
KitaPDev <kita.pairojtanachai@gmail.com>
2019-10-14 14:26:00 +0700

    add android client

--- server/server.py
+++ server/server.py
@@ -15,15 +15,12 @@ class ServerThread(threading.Thread):
         print("Running thread ", self.getName())
         receivedData = clientSocket.recv(1024).decode()
         print(receivedData)
-        self.closeConnection()
-
-    def closeConnection(self):
         clientSocket.send("Hello Client!".encode())
         clientSocket.close()
 
 
 TCP_IP = '0.0.0.0'
-TCP_PORT = 40404
+TCP_PORT = 54321
 
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -32,8 +29,11 @@ serverSocket.bind((TCP_IP, TCP_PORT))
 print("Server started")
 print("Waiting on port", TCP_PORT)
 
+dictClientAddress_ThreadName = {}
+
 while True:
     serverSocket.listen(1)
     (clientSocket, clientAddress) = serverSocket.accept()
     newClientThread = ServerThread(clientAddress, clientSocket)
+    dictClientAddress_ThreadName.update(clientAddress, newClientThread.getName())
     newClientThread.start()
--- server/test/test_client.py
+++ server/test/test_client.py
@@ -1,7 +1,7 @@
 import socket
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientSocket.connect(('192.168.1.64', 40404))
+clientSocket.connect(('192.168.1.108', 54321))
 clientSocket.send('Hello Server!'.encode())
 print(clientSocket.recv(1024).decode())
 
