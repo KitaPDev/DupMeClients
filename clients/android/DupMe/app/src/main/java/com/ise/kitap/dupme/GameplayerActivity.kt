f8c2e3c6d294599d87fc7c6b76a672fa6b60cf3a
prime <primepianpucktr@gmail.com>
2019-10-17 21:11:20 +0700

    Add gameplayer activity and piano button

---
+++ clients/android/DupMe/app/src/main/java/com/ise/kitap/dupme/GameplayerActivity.kt
@@ -0,0 +1,53 @@
+package com.ise.kitap.dupme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class GameplayerActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gameplayer)
+
+        val A3: Button = findViewById(R.id.A3)
+        val B3: Button = findViewById(R.id.B3)
+        val C3: Button = findViewById(R.id.C3)
+        val C4: Button = findViewById(R.id.C4)
+        val D3: Button = findViewById(R.id.D3)
+        val E3: Button = findViewById(R.id.E3)
+        val F3: Button = findViewById(R.id.F3)
+        val G3: Button = findViewById(R.id.G3)
+
+        A3.setOnClickListener(View.OnClickListener {
+
+        })
+        B3.setOnClickListener(View.OnClickListener {
+
+        })
+        C3.setOnClickListener(View.OnClickListener {
+
+        })
+        C4.setOnClickListener(View.OnClickListener {
+
+        })
+        D3.setOnClickListener(View.OnClickListener {
+
+        })
+        E3.setOnClickListener(View.OnClickListener {
+
+        })
+        F3.setOnClickListener(View.OnClickListener {
+
+        })
+        G3.setOnClickListener(View.OnClickListener {
+
+        })
+
+
+
+    }
+}
