db0a81c757b3c229048bd52d9555df695273fa32
KitaPDev <kita.pairojtanachai@gmail.com>
2019-10-19 20:33:46 +0700

    'reformat code + fixed MainActivity code'

--- android/DupMe/app/src/main/java/com/ise/kitap/dupme/MainActivity.kt
+++ android/DupMe/app/src/main/java/com/ise/kitap/dupme/MainActivity.kt
@@ -3,27 +3,29 @@ package com.ise.kitap.dupme
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val username: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button: Button = findViewById(R.id.button)
-        val editText: EditText = findViewById(R.id.editText)
-        button.setOnClickListener(View.OnClickListener {
-            openGameplayer()
-        })
-
-
+        btnFindMatch.setOnClickListener {
+            if(verifyUserInput()) {
+                openGamePlayer()
+            }
+        }
     }
 
-    private fun openGameplayer() {
-        val intent = Intent(this, GameplayerActivity::class.java)
+    private fun verifyUserInput(): Boolean {
+        return edtUsername.text.isNotBlank()
+    }
+
+    private fun openGamePlayer() {
+        val intent = Intent(this, GamePlayerActivity::class.java)
         startActivity(intent)
     }
 }
