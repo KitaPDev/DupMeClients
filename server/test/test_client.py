f92537cf0b6c2d2ffe61e1b6c877b304cd2e8dc6
KitaPDev <kita.pairojtanachai@gmail.com>
2019-10-19 21:06:27 +0700

    revert

--- server/test/test_client.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientSocket.connect(('192.168.1.108', 54321))
-clientSocket.send('Hello Server!'.encode())
-print(clientSocket.recv(1024).decode())
-
-clientSocket.close()
